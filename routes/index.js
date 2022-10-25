968c7cfb6781c250815524f829532784f13db8eb
aizealawin <ktmudalige@gmail.com>
2022-10-25 12:57:15 -0400

    created createUser and deletegame controllers

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,13 @@ const router = Router()
 const controllers = require('../controllers')
 
 router.get('/', (req, res) => res.send('This is root!'))
+
 router.post('/createVideoGame', controllers.createVideoGame)
-router.get('/login', (req, res) => res.send('This is login!'))
+router.get('/deleteGame', controllers.deleteVideoGame)
+
+router.get('/login', controllers.createUser)
 
 router.get('/library', (req, res) => res.send('This is your library'))
-
-router.get('/addNewGame', (req, res) =>
-  res.send('This is where you add a new game')
-)
-
-router.get('/deleteGame', (req, res) =>
-  res.send('This is where you delete a game!')
-)
 
 router.get('/updateLibrary', (req, res) =>
   res.send('This is where you update games.')
